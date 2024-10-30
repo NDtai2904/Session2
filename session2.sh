fa051840ae03f403196d9e3625b39bfb024d76b5
DuyTai - Devop <nguyenduytai2904.learn@gmail.com>
2024-10-30 07:20:40 +0000

    Lab session2: scripting and automation

---
+++ session2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# giải nén thư mục zip
+unzip session2.zip 
+
+# đường dẫn cho file giải nén
+path="session2"
+
+
+for pdf_file in "$path"/*.pdf; do
+    
+    if [ ! -f "$pdf_file" ]; then
+        echo "Không có file PDF nào trong thư mục $path."
+        exit 1
+    fi
+
+    # lấy tên file không lấy phần mở rộng pdf
+    name_file=$(basename "$pdf_file" .pdf)
+
+    # chuyển pdf sang dạng text để xử lí
+    pdftotext "$pdf_file" "$path/${name_file}.txt"
+
+    # tách từ và lấy các từ duy nhất
+    # câu lệnh tham khảo từ stackoverflow
+    unique_words=$(tr ' ' '\n' < "$path/${name_file}.txt" | grep -oE '^[a-zA-Z]+' | sort -fu)
+
+    # tạo thư mục tương ứng với các file .pdf
+    mkdir -p "$path/$name_file"
+
+    
+    for word in $unique_words; do
+        # Tạo file với tên từ và nội dung là từ đó
+        echo "$word" > "$path/$name_file/$word.txt"
+    done
+    
+    echo "Đã xử lý: $pdf_file -> $path/$name_file/"
+done
+
+rm -rf "$path"/*.txt
+
+echo "Hoàn tất!"
+
